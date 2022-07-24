a241a9d8cb6e768858e8c5a1e7f90bd78c5ce1ac
anjalisheel <anjalisheel@gmail.com>
2022-07-24 23:20:57 +0530

    Add files via upload

---
+++ 09-07-2022/Program 1/Program.java
@@ -0,0 +1,8 @@
+public class Program {
+    public static void main(String[] args) {
+        Person person = new Person("abc", 30, "male");
+        System.out.println(person.name);
+        System.out.println(person.gender);
+        System.out.println(person.age);
+    }
+}

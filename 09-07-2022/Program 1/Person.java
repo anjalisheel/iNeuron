78c59cf0694f698986fdd7e2b56ffb732d7161c5
anjalisheel <anjalisheel@gmail.com>
2022-07-24 23:20:35 +0530

    Add files via upload

---
+++ 09-07-2022/Program 1/Person.java
@@ -0,0 +1,12 @@
+public class Person {
+    String name;
+    int age;
+    String gender;
+
+    public Person(String name, int age, String gender)
+    {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+}

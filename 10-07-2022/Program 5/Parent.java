c8f6215342fc54dd703abf7522f2d58225819ead
anjalisheel <anjalisheel@gmail.com>
2022-07-25 00:57:49 +0530

    Add files via upload

---
+++ 10-07-2022/Program 5/Parent.java
@@ -0,0 +1,10 @@
+public class Parent {
+    public Parent()
+    {
+        System.out.println("Default ctor");
+    }
+    public Parent(int value)
+    {
+        System.out.println("Parent class ctor : " + value);
+    }
+}

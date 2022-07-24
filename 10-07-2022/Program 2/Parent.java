6ac466002336db7489b2ef3a135e885398c59143
anjalisheel <anjalisheel@gmail.com>
2022-07-25 00:56:25 +0530

    Add files via upload

---
+++ 10-07-2022/Program 2/Parent.java
@@ -0,0 +1,6 @@
+public class Parent {
+    public Parent()
+    {
+        System.out.println("Parent class constructor");
+    }
+}
---
+++ 10-07-2022/Program 2/Program.java
@@ -0,0 +1,5 @@
+public class Program {
+    public static void main(String[] args) {
+        Child obj = new Child();
+    }
+}

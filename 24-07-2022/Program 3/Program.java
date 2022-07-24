00dd76f924b3ec534b0a6327f424302000a89c01
anjalisheel <anjalisheel@gmail.com>
2022-07-24 23:22:45 +0530

    Add files via upload

---
+++ 24-07-2022/Program 3/Program.java
@@ -0,0 +1,16 @@
+public class Program {
+    public static void main(String[] args) {
+        Class1 obj1 = new Class1();
+        obj1.method1();
+        System.out.println();
+
+        Class2 obj2 = new Class2();
+        obj2.method1();
+        System.out.println();
+
+        Class3 obj3 = new Class3();
+        obj3.method1();
+        obj3.method2();
+        obj3.method3();
+    }
+}

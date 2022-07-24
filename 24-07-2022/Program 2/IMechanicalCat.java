255b6246e00dff6d6b8adfe40ef735da6df73b83
anjalisheel <anjalisheel@gmail.com>
2022-07-24 23:18:57 +0530

    Add files via upload

---
+++ 24-07-2022/Program 2/IMechanicalCat.java
@@ -0,0 +1,3 @@
+public interface IMechanicalCat {
+    public void makeSound();
+}
---
+++ 24-07-2022/Program 2/ToyCat.java
@@ -0,0 +1,6 @@
+public class ToyCat implements IMechanicalCat {
+    @Override
+    public void makeSound() {
+        System.out.println("Makes mechanical meow sound.");
+    }
+}

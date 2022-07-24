0d834bad6d84e9cc28552c39514896b68101b316
anjalisheel <anjalisheel@gmail.com>
2022-07-25 00:56:51 +0530

    Add files via upload

---
+++ 10-07-2022/Program 3/Program.java
@@ -0,0 +1,9 @@
+public class Program {
+    public static void main(String[] args) {
+        Parent parentObj = new Parent();
+        parentObj.Print();
+
+        Child childObj = new Child();
+        childObj.Print();
+    }
+}

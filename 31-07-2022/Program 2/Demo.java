8f476d24addb47a0cabea9190263ab87503eefe0
anjalisheel <anjalisheel@gmail.com>
2022-07-31 22:38:52 +0530

    Add files via upload

---
+++ 31-07-2022/Program 2/Demo.java
@@ -0,0 +1,11 @@
+public class Demo {
+    public Demo()
+    {
+        System.out.println("Demo : constructor");
+    }
+
+    public void printDemo()
+    {
+        System.out.println("Print demo");
+    }
+}
---
+++ 31-07-2022/Program 2/SingletonExample.java
@@ -0,0 +1,23 @@
+public class SingletonExample {
+    private SingletonExample()
+    {
+        System.out.println("Private Constructor");
+    }
+
+    private static SingletonExample instance = new SingletonExample();
+
+    public static SingletonExample getInstance()
+    {
+        return instance;
+    }
+
+    public void printMessage(String message)
+    {
+        System.out.println("Message : " + message);
+    }
+
+    public static void printStaticMessage(String message)
+    {
+        System.out.println("Static Message : " + message);
+    }
+}

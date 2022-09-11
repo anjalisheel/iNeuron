6fe0e7ba4f007ad39e3db3e6b7c84e14eb725f7e
anjalisheel <anjalisheel@gmail.com>
2022-09-11 23:14:04 +0530

    Delete 09-07-2022/Program 1 directory

--- 09-07-2022/Program 1/Program.java
+++
@@ -1,8 +0,0 @@
-public class Program {
-    public static void main(String[] args) {
-        Person person = new Person("abc", 30, "male");
-        System.out.println(person.name);
-        System.out.println(person.gender);
-        System.out.println(person.age);
-    }
-}
